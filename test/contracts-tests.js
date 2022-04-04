f6bd23137a5a2595a9b3bfe29b85a81a4e7cb2fb
daniecon <daniel.contreras@math.ethz.ch>
2022-04-04 12:15:34 +0200

    More tests added and renamed file

--- test/contracts-tests.js
+++ test/contracts-tests.js
@@ -1,13 +1,23 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
-describe("Create Voting", function () {
-  it("Should create a new voting with 2 candidates", async function () {
-    const Main = await ethers.getContractFactory("Main")
-    const mainContract = await Main.deploy()
-    await mainContract.deployed()
+describe("Main contract tests", async function () {
+  let Main
+  let mainContract
+  before(async function () {
+    Main = await ethers.getContractFactory("Main")
+  })
 
+  it("Should deploy the main contract", async function () {
+    mainContract = await Main.deploy()
+    await mainContract.deployed()
+  })
+
+  it("Should start with 0 deployed votings", async function () {
     expect(await mainContract.votingId()).to.equal(0)
+  })
+
+  it("Should create a new voting with 2 candidates and increase votingId", async function () {
 
     const block = await ethers.provider.getBlockNumber()
     const time = (await ethers.provider.getBlock(block)).timestamp
@@ -18,11 +28,11 @@ describe("Create Voting", function () {
       time + 3600,
       time + 360000,
       ["Candidate A", "Candidate B"]
-    );
+    )
 
     // wait until the transaction is mined
     await createVotingTx.wait();
 
     expect(await mainContract.votingId()).to.equal(1);
-  });
-});
+  })
+})

d09e8b29fc68165a2a8466a66124804e32f2bd29
daniecon <daniel.contreras@math.ethz.ch>
2022-04-08 12:00:37 +0200

    Added connection to RPC

--- src/utils/web3Connect.js
+++ src/utils/web3Connect.js
@@ -3,11 +3,23 @@ import Web3Modal from 'web3modal'
 import contractJson from '../artifacts/contracts/Main.sol/Main.json'
 import { mainAddress } from './contractAddress'
 
+export function web3Load() {
+    const provider = new ethers.providers.JsonRpcProvider('https://api.avax-test.network/ext/bc/C/rpc')
+    const mainContract = new ethers.Contract(mainAddress, contractJson.abi, provider)
+    return { mainContract: mainContract, provider: provider }
+}
+
 export async function web3Connect() {
-    const web3Modal = new Web3Modal()
+    const providerOptions = {}
+    const web3Modal = new Web3Modal({
+        network: 'fuji',
+        cacheProvider: true,
+        providerOptions
+    })
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(mainAddress, contractJson.abi, signer)
-    return { contract: contract, provider: provider, account: signer }
+    const account = await signer.getAddress()
+    return { contract: contract, provider: provider, account: account }
 }

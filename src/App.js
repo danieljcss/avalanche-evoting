f689f2dd85b22a8324a735cf35b3d164f8e1ac8b
daniecon <daniel.contreras@math.ethz.ch>
2022-04-08 12:02:56 +0200

    Connection madee from Main App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
-import { web3Connect } from './utils/web3Connect'
-import { Routes, Route, Link } from "react-router-dom"
+import { web3Connect, web3Load } from './utils/web3Connect'
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/navbar"
 import ActiveVotings from "./pages/activeVotings"
 
 
@@ -8,21 +9,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: "",
-      mainInstance: null,
+      account: null,
+      provider: web3Load().provider,
+      mainInstance: web3Load().mainContract,
     }
   }
 
-  componentDidMount() {
-    this.init()
-  }
-
-  async init() {
+  async connect(e) {
+    e.preventDefault()
     try {
       const connect = await web3Connect()
-      const account = await connect.account.getAddress()
       this.setState({
-        account: account,
+        account: connect.account,
+        provider: connect.provider,
         mainInstance: connect.contract
       })
     } catch (error) {
@@ -33,13 +32,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar account={this.state.account} />
+        <Navbar account={this.state.account} connect={this.connect.bind(this)} />
         {/*For routing through the react application*/}
         <Routes>
           {/* Route to Active voting page */}
           <Route
             path="/"
-            element={<ActiveVotings account={this.state.account} />}
+            element={<ActiveVotings
+              account={this.state.account}
+              provider={this.state.provider}
+              mainInstance={this.state.mainInstance}
+            />}
           />
         </Routes>
       </div>
@@ -47,38 +50,6 @@ class App extends Component {
   }
 }
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav
-        className="navbar navbar-dark shadow"
-        id="navbar"
-      >
-        {/* Logo and Title  */}
-        <div className="col-8 align-middle">
-          <img src="/logoAV.svg" alt="" height="50px" width="50px" id="logo" className="inline-block align-middle" />
-          <span className="inline-block align-middle" id="logo-title">
-            <Link to="/" >
-              Avalanche Votings
-            </Link>
-          </span>
-        </div>
 
-        {/* Account address on the right side of the navbar  */}
-        <div className="col-4">
-          <div id="address-nav" className="d-flex justify-content-end link-address">
-            <a
-              href={`https://testnet.snowtrace.io/address/${this.props.account}`}
-              target="_blank" rel="noopener noreferrer"
-            >
-              {`${this.props.account.slice(0, 10)}...`}
-            </a>
-          </div>
-        </div>
-
-      </nav >
-    );
-  }
-}
 
 export default App;
